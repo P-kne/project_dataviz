2ec88ae536d7b635168faf70f263db6aa48bac6a
mohamedBenma <mbenmaouche09@gmail.com>
2023-12-20 16:00:41 +0100

    upload & save

--- project_dataviz/src/components/HelloWorld.vue
+++ project_dataviz/src/components/HelloWorld.vue
@@ -10,11 +10,15 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(row, index) in data" :key="index">
-            <td v-for="(value, key) in row" :key="key">{{ value }}</td>
+          <tr v-for="(row, rowIndex) in data" :key="rowIndex">
+            <td v-for="(value, key) in row" :key="key">
+              <input v-model="data[rowIndex][key]" />
+            </td>
           </tr>
         </tbody>
       </table>
+      <button @click="saveChanges">Enregistrer localement</button>
+      <button @click="saveToLocalCSV">Télécharger CSV</button>
     </div>
   </div>
 </template>
@@ -26,6 +30,7 @@ export default {
   data() {
     return {
       data: [],
+      originalData: [],
     };
   },
   methods: {
@@ -33,12 +38,62 @@ export default {
       const file = event.target.files[0];
       Papa.parse(file, {
         complete: (result) => {
-          // Les données CSV sont stockées dans result.data
           this.data = result.data;
+          this.originalData = JSON.parse(JSON.stringify(result.data));
         },
-        header: true, // Si la première ligne du CSV contient les noms de colonnes
+        header: true,
       });
+    },
+    saveChanges() {
+      // Logique pour sauvegarder les modifications localement
+      localStorage.setItem('csvData', JSON.stringify(this.data));
+      alert('Modifications enregistrées localement.');
+    },
+    saveToLocalCSV() {
+      // Convertir les données en format CSV
+      const csv = Papa.unparse(this.data);
+      
+      // Créer un objet Blob pour stocker le contenu CSV
+      const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+
+      // Générer un lien d'ancrage pour le téléchargement
+      const link = document.createElement('a');
+      const url = URL.createObjectURL(blob);
+      link.href = url;
+      link.setAttribute('download', 'modified_data.csv');
+      document.body.appendChild(link);
+
+      // Déclencher le téléchargement et supprimer le lien d'ancrage
+      link.click();
+      document.body.removeChild(link);
     },
   },
 };
 </script>
+
+<style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
+
+  th,
+  td {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  tbody tr:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+
+  input[type="file"] {
+    margin-bottom: 10px;
+  }
+</style>
